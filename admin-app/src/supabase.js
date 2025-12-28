06b32c6fc5977a07332591f570d3845d37c16aa2
abnvgurram <abnv.gurram2905@gmail.com>
2025-12-28 08:09:07 +0530

    reset

--- admin-app/src/supabase.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://pklhwfipldiswdboobua.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrbGh3ZmlwbGRpc3dkYm9vYnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2NzM3NTEsImV4cCI6MjA4MjI0OTc1MX0.A9Omf4jyyj8U1AF163GWHlvQC5BNeiuSPV1iaiXCGQc';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
